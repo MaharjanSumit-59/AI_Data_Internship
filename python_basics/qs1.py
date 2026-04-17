d383102ffc4745b0f03ceb2dbd1976dae92d8b04
MaharjanSumit-59 <sumitmahz@gmail.com>
2026-04-17 13:33:56 +0545

    Initialization

---
+++ python_basics/qs1.py
@@ -0,0 +1,9 @@
+"""
+Write a Python program that takes a student's marks as input and prints the grade based on the following criteria:
+90 and above → A
+80 to 89 → B
+70 to 79 → C
+60 to 69 → D
+Below 60 → Fail
+"""
+
