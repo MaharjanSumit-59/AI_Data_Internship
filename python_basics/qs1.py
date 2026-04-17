95cd9fc7a861137205061555644bf879547e3dd7
MaharjanSumit-59 <sumitmahz@gmail.com>
2026-04-17 13:47:29 +0545

    first question

--- python_basics/qs1.py
+++ python_basics/qs1.py
@@ -6,4 +6,17 @@ Write a Python program that takes a student's marks as input and prints the grad
 60 to 69 → D
 Below 60 → Fail
 """
-
+try:
+    marks = float(input("Enter the student's marks: "))
+    if marks >= 90:
+        print("Grade: A")
+    elif marks >= 80:
+        print("Grade: B")
+    elif marks >= 70:
+        print("Grade: C")
+    elif marks >= 60:
+        print("Grade: D")
+    else:
+        print("Grade: Fail")
+except ValueError:
+    print("Invalid input. Please enter a valid number.")
