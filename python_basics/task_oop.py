00ed092d41c291b7b2e34daa8b2396e930acda59
MaharjanSumit-59 <sumitmahz@gmail.com>
2026-04-19 19:21:35 +0545

    Inheritance

--- python_basics/task_oop.py
+++ python_basics/task_oop.py
@@ -108,3 +108,68 @@ manager.validate_password("my_secure_password") # Valid password
 manager.validate_password("wrong_password") # Invalid password
 print(manager.__password) # This will raise an AttributeError because __password is private and cannot be accessed directly.
 """
+
+# Create a base class Animal with a method sound(). Derive classes like Dog and Cat that override it.
+"""
+class Animal:
+    # there is no need to create an init method in the base class because we are not initializing any attributes in the base class.
+    def sound(self):
+        print("Animal makes a sound.")
+    
+class Dog(Animal):
+    def sound(self):
+        print("Dog barks.")
+class Cat(Animal):
+    def sound(self):
+        print("Cat meows.")
+        
+dog = Dog()
+cat = Cat()
+animal = Animal()
+animal.sound() # Output: Animal makes a sound.
+dog.sound() # Output: Dog barks.
+cat.sound() # Output: Cat meows.    
+"""
+
+# Write a class Employee and a subclass Manager that adds extra attributes like bonus.
+"""
+class Employee:
+    def __init__(self, name, salary):
+        self.name = name
+        self.salary = salary
+
+class Manager(Employee):
+    def __init__(self, name, salary, bonus):
+        super().__init__(name, salary) # calling the init method of the base class to initialize name and salary attributes.
+        self.bonus = bonus
+        
+manager = Manager("John", 50000, 10000)
+print(f"Manager's name: {manager.name}")    
+print(f"Manager's salary: {manager.salary}")
+print(f"Manager's bonus: {manager.bonus}")      
+"""
+
+# Create a class Shape and derive Circle and Square with their own area methods.
+"""
+class shape:
+    def area(self):
+        if type(self) == Circle: # checking the type of the object to determine which area method to call.
+            return 3.14 * self.radius ** 2
+        else:
+            return self.side ** 2
+class Circle(shape):
+    def __init__(self, radius):
+        self.radius = radius
+    def area(self):
+        return super().area()
+class Square(shape):
+    def __init__(self, side):
+        self.side = side
+    def area(self):
+        return super().area()
+    
+circle = Circle(5)
+square = Square(4)
+print(f"Area of the circle: {circle.area()}") # Output: Area of the circle: 78.5
+print(f"Area of the square: {square.area()}") # Output: Area of the square: 16
+"""
